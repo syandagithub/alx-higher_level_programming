465f5effefa4b39fed44f2c9ad98ea9f54f96fa5
syanda <syandamnt@gmail.com>
2023-12-04 10:15:40 -0800

    remove

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-
-def lookup(obj):
-    ''' function: lookup()
-    Returns a list object
-    '''
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-
-class BaseGeometry:
-    ''' Class: BaseGeometry
-    '''
-    pass
