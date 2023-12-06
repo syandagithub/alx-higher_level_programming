ab821be7054e28ceee7e077b5d9681364db3fc3a
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2023-12-06 08:30:10 +0200

    Add files via upload

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
