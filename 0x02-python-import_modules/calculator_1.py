c079acfbd6a458d195599dbbeda3a5606dbbf5fa
SYANDA MNTAMBO's GITHUB <syandamnt@gmail.com>
2023-12-20 16:04:16 +0200

    new
    new

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def add(a, b):
+   
+    return (a + b)
+
+
+def sub(a, b):
+
+    return (a - b)
+
+
+def mul(a, b):
+  
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
