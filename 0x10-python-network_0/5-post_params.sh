fa874bfc9435769b7a2a6fcdd95411ced6255f72
syanda <syandamnt@gmail.com>
2024-02-29 10:16:36 -0800

    eyy

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes in a URLm and sends a POST request with specific parameters
-curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
-
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
