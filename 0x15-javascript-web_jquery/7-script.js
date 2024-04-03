b88f3e027144788f03fbc0f69f21f0c094873c67
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-04-03 21:06:21 +0200

    7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-let url = 'https://swapi.co/api/people/5/?format=json';
+let url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 $.get(url, function (data, stat) {
   $('div#character').text(data.name);
 });
